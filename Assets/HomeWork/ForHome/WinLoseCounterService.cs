e4a895664d18631512cb6b11bf1a0960f6feb607
VOLW11 <oleg.votincev18@gmail.com>
2024-12-16 17:59:21 +0300

    Update9_1612

---
+++ Assets/HomeWork/ForHome/WinLoseCounterService.cs
@@ -0,0 +1,21 @@
+using Assets.HomeWork.Develop.CommonServices.DataManagment.DataProviders;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.HomeWork.ForHome
+{
+    public class WinLoseCounterService : IDataReader<PlayerData>, IDataWriter<PlayerData>
+    {
+        public void ReadFrom(PlayerData data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteTo(PlayerData data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
